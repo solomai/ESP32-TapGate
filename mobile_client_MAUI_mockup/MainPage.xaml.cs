47a5c9c5394343f4bd31ce37a8f308926407ee1a
solomai <solomaj@gmail.com>
2025-09-06 22:44:55 +0300

    mainpage refactored

--- mobile_client_MAUI_mockup/MainPage.xaml.cs
+++ mobile_client_MAUI_mockup/MainPage.xaml.cs
@@ -10,8 +10,38 @@ public partial class MainPage : ContentPage
     {
         base.OnSizeAllocated(width, height);
         bool isLandscape = width > height;
-        LandscapeLayout.IsVisible = isLandscape;
-        PortraitLayout.IsVisible = !isLandscape;
+        UpdateLayout(isLandscape);
+    }
+
+    void UpdateLayout(bool isLandscape)
+    {
+        ContentGrid.RowDefinitions.Clear();
+        ContentGrid.ColumnDefinitions.Clear();
+
+        if (isLandscape)
+        {
+            ContentGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            ContentGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+            ContentGrid.RowSpacing = 0;
+            ContentGrid.ColumnSpacing = 40;
+
+            Grid.SetRow(StatusLayout, 0);
+            Grid.SetColumn(StatusLayout, 0);
+            Grid.SetRow(ActionButton, 0);
+            Grid.SetColumn(ActionButton, 1);
+        }
+        else
+        {
+            ContentGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            ContentGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+            ContentGrid.RowSpacing = 40;
+            ContentGrid.ColumnSpacing = 0;
+
+            Grid.SetRow(StatusLayout, 0);
+            Grid.SetColumn(StatusLayout, 0);
+            Grid.SetRow(ActionButton, 1);
+            Grid.SetColumn(ActionButton, 0);
+        }
     }
     void OnDoAction(object sender, EventArgs e)
     {
