ecf0ba4527349fab88b81d3a56680990c7d84898
solomai <solomaj@gmail.com>
2025-09-13 17:16:36 +0300

    Add components base

---
+++ esp32_mcu/components/common/types.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdint.h>
+#include <inttypes.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// nonce type
+typedef uint32_t nonce_t;
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ esp32_mcu/main/main.c
@@ -0,0 +1,12 @@
+// include freertos lib
+#include "esp_log.h"
+
+// include components
+#include "types.h"
+
+void app_main(void)
+{
+    // TEST
+    nonce_t nonce = 0;
+    ESP_LOGI("MAIN", "NONCE: %lu", nonce);
+}
