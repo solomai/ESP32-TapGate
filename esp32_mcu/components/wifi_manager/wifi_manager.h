1d6f448ea2447f884c8e789c47ef5039dc2637d6
solomai <solomaj@gmail.com>
2025-09-20 01:22:09 +0300

    component wifi manager skeleton

---
+++ esp32_mcu/components/wifi_manager/wifi_manager.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// Defines the maximum size of a SSID name. Default: 32 is IEEE standard.
+#define MAX_SSID_SIZE						32
+
+// Defines the maximum size of a WPA2 passkey. Default: 64 is IEEE standard.
+#define MAX_PASSWORD_SIZE					64
+
+// Defines the maximum number of access points that can be scanned.
+#define MAX_AP_NUM 							15
