cd7fc27a709b91df880d637b7c6e5c0c7184d7b7
solomai <solomaj@gmail.com>
2025-09-20 20:29:21 +0300

    Update nvm_partition.h

--- esp32_mcu/components/common/nvm/nvm_partition.h
+++ esp32_mcu/components/common/nvm/nvm_partition.h
@@ -2,7 +2,14 @@
 
 // keep in sync with partitions.csv
 
+#ifndef NVM_PARTITION_DEFAULT
 #define NVM_PARTITION_DEFAULT "nvs"
-#define NVM_PARTITION_CLIENTS "nvs_entity"
-#define NVM_PARTITION_NONCE   "nvs_nonce"
+#endif
 
+#ifndef NVM_PARTITION_CLIENTS
+#define NVM_PARTITION_CLIENTS "nvs_entity"
+#endif
+
+#ifndef NVM_PARTITION_NONCE
+#define NVM_PARTITION_NONCE   "nvs_nonce"
+#endif
