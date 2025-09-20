3836b63edfdd28fbcfc0f37fd76b74ef4bab21f2
solomai <solomaj@gmail.com>
2025-09-20 17:14:41 +0300

    include fix for http server

--- esp32_mcu/components/admin_portal/http_server.c
+++ esp32_mcu/components/admin_portal/http_server.c
@@ -1,11 +1,12 @@
 #include "http_server.h"
+#include "esp_http_server.h"
 
 httpd_handle_t httpserver_handle = NULL;
 
 esp_err_t http_server_start()
 {
     if (httpserver_handle != NULL) {
-        return ESP_ERR_
+        return ESP_ERR_INVALID_STATE;
     }
     return ESP_OK;
 }
