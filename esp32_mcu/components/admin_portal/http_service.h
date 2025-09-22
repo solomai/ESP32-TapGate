1508ea5b160a7409afbc4727267cf038c781eb66
solomai <solomaj@gmail.com>
2025-09-22 21:21:07 +0300

    Define webapp location for HTTP service

--- esp32_mcu/components/admin_portal/http_service.h
+++ esp32_mcu/components/admin_portal/http_service.h
@@ -3,6 +3,8 @@
 #include "esp_err.h"
 #include "esp_http_server.h"
 
+#define WEBAPP_LOCATION "/"
+
 esp_err_t http_service_handle_get(httpd_req_t *req);
 esp_err_t http_service_handle_post(httpd_req_t *req);
 esp_err_t http_service_handle_delete(httpd_req_t *req);
