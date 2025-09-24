102cdb8a1087a696c259675ab8157ddfdb2949e7
solomai <solomaj@gmail.com>
2025-09-25 00:27:03 +0300

    Increase HTTP header limit for admin portal

--- esp32_mcu/components/admin_portal/http_server.c
+++ esp32_mcu/components/admin_portal/http_server.c
@@ -16,6 +16,8 @@ esp_err_t http_server_start()
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.max_uri_handlers = 32;
     config.lru_purge_enable = true;
+    // Browsers can send large Cookie headers; bump the limit to avoid 431 errors.
+    config.max_req_hdr_len = 1024;
 
     esp_err_t err = httpd_start(&httpserver_handle, &config);
     if (err != ESP_OK)
