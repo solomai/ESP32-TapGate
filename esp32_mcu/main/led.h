bde1cb6d5e293662d75b4d899935fc5ba2cc32aa
solomai <solomaj@gmail.com>
2025-09-28 19:05:29 +0300

    Update led.h

--- esp32_mcu/main/led.h
+++ esp32_mcu/main/led.h
@@ -4,12 +4,6 @@
 #define BLUE_LED_PIN GPIO_NUM_2
 #endif
 
-void blue_led_init()
-{
-    gpio_reset_pin(BLUE_LED_PIN);
-    gpio_set_direction(BLUE_LED_PIN, GPIO_MODE_OUTPUT);
-}
-
 void turn_on_blue_led()
 {
     gpio_set_level(BLUE_LED_PIN, 1);
@@ -18,4 +12,11 @@ void turn_on_blue_led()
 void turn_off_blue_led()
 {
     gpio_set_level(BLUE_LED_PIN, 0);
+}
+
+void blue_led_init()
+{
+    gpio_reset_pin(BLUE_LED_PIN);
+    gpio_set_direction(BLUE_LED_PIN, GPIO_MODE_OUTPUT);
+    turn_off_blue_led();
 }
