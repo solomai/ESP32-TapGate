34cd800f47b58db641b3c44deac93b9678df120e
solomai <solomaj@gmail.com>
2026-01-01 19:12:55 +0200

    Update types.h

--- device_firmware/main/types.h
+++ device_firmware/main/types.h
@@ -4,10 +4,12 @@
 #include <string_view>
 #include <cstdint>
 
-
+// Types
 using uuid_t = std::array<uint8_t, 16>;
-
-
 
 // Constants
 static constexpr std::size_t DEVICE_NAME_CAPACITY = 32;
+
+// Other
+#define STR_IMPL(x) #x
+#define STR(x) STR_IMPL(x)
