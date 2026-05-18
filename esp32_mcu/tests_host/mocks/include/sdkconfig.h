5c994faebaad52a6dcd26973f1d6c956e2029299
solomai <solomaj@gmail.com>
2026-05-18 21:14:33 +0300

    removed outdated mcu code

--- esp32_mcu/tests_host/mocks/include/sdkconfig.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/*
- * Dummy sdkconfig header for host-unit tests. Production firmware includes the
- * generated configuration from ESP-IDF; host tests provide only the options
- * that are actually required by the compiled sources.
- */
