52add0f4904ddf406cfbbd08318d0cc858fff970
solomai <solomaj@gmail.com>
2026-05-18 19:47:43 +0300

    log events reordered for main loop

--- tapgate_firmware/main/main.cpp
+++ tapgate_firmware/main/main.cpp
@@ -59,14 +59,6 @@ extern "C" void app_main(void)
 
     // Almost all initialization steps are complete. 
     // Report startup complete before entering main loop.
-#ifdef APP_DEBUG_MODE
-    // Report debug mode enabled - this is a warning since it may impact security and performance,
-    // but it's not an error since it's intentional in some cases (e.g. development builds).
-    EVENT_JOURNAL_ADD(EVENT_JOURNAL_WARNING,
-        TAG_MAIN,
-        "Debug Mode enabled");
-#endif
-
     {
         // Initialization complete. Log app info
 
@@ -88,6 +80,14 @@ extern "C" void app_main(void)
             app_desc->idf_ver,
             get_reset_reason_text(reason),
             DateTime.FormatFixed<32>(DT_FMT_HUMAN_FULL).data());
+
+#ifdef CONFIG_APP_DEBUG_MODE
+    // Report debug mode enabled - this is a warning since it may impact security and performance,
+    // but it's not an error since it's intentional in some cases (e.g. development builds).
+    EVENT_JOURNAL_ADD(EVENT_JOURNAL_WARNING,
+        TAG_MAIN,
+        "Debug Mode enabled");
+#endif           
     }
     
     // Main app loop
